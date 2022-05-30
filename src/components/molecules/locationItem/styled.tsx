f14d20044338ab303027c6d5101cdf16ba190621
nikolay <gugalohikolaychur@gmail.com>
2022-05-30 11:57:06 +0300

    add location item

---
+++ src/components/molecules/locationItem/styled.tsx
@@ -0,0 +1,10 @@
+import styled from 'styled-components/native'
+
+export const StyledLocationItem = styled.View`
+  width: 163px;
+  min-height: 80px;
+  padding: 12px;
+  border-radius: 8px;
+  margin-right: 17px;
+  margin-bottom: 20px;
+`
