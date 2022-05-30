45b0612e9cf92694f6ec9c2eed020b5a54ed9407
nikolay <gugalohikolaychur@gmail.com>
2022-05-30 12:51:51 +0300

    add locations

--- src/components/molecules/locationItem/styled.tsx
+++ src/components/molecules/locationItem/styled.tsx
@@ -1,10 +1,15 @@
 import styled from 'styled-components/native'
 
-export const StyledLocationItem = styled.View`
+import { colors } from 'src/theme/colors'
+
+export const StyledLocationItem = styled.View.attrs({
+  borderRadius: 8,
+})`
   width: 163px;
   min-height: 80px;
   padding: 12px;
-  border-radius: 8px;
-  margin-right: 17px;
   margin-bottom: 20px;
+  border: 1px solid ${colors.basic.line};
+  margin-right: 8.5px;
+  margin-left: 8.5px;
 `
--- src/modules/location/index.tsx
+++ src/modules/location/index.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import { Text, View } from 'react-native'
 
+import { Locations } from 'src/components/organisms/locations'
+
 export const LocationScreen = () => {
   return (
     <View>
-      <Text>Location</Text>
+      <Locations />
     </View>
   )
 }
-
